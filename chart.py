0c8ef3825a845a0c36f8c90b5faa9f65b40cb015
pragasto <pragasto.aji@efishery.com>
2025-03-16 17:54:25 +0700

    fix/revised

--- chart.py
+++ chart.py
@@ -9,10 +9,10 @@ def get_chart_pce(data):
     )
 
     lines = (
-        alt.Chart(data, title="Grafik Real PCE AS VS Prediksi PCE AS Jika Tidak Terjadi Pandemi COVID-19",height=360).mark_line().encode(
-            x=alt.X("date:T",title='Bulan',),
+        alt.Chart(data, title="Real US PCE VS US PCE Prediction if COVID-19 Pandemic Had not Occured",height=360).mark_line().encode(
+            x=alt.X("date:T",title='Month',),
             #="value:Q",
-            y=alt.Y('value:Q', scale=alt.Scale(domain=[12000, 17500]),title='Miliar USD'),
+            y=alt.Y('value:Q', scale=alt.Scale(domain=[12000, 17500]),title='Billion USD'),
             color="variable:N",
             strokeDash="variable:N",
         )
@@ -20,11 +20,11 @@ def get_chart_pce(data):
 
     points = lines.transform_filter(hover).mark_circle(size=65)
 
-    base = alt.Chart(data).encode(x=alt.X("date:T",title='Bulan'))
+    base = alt.Chart(data).encode(x=alt.X("date:T",title='Month'))
     columns = sorted(data.variable.unique())
     tooltips = base.transform_pivot('variable', value='value', groupby=['date']).mark_rule().encode(
                     opacity=alt.condition(hover, alt.value(0.3), alt.value(0)),
-                    tooltip=[alt.Tooltip(c, type='quantitative',format=',.0f',) for c in columns]+[alt.Tooltip("date:T", title="Bulan",format = ("%b %Y"),)],
+                    tooltip=[alt.Tooltip(c, type='quantitative',format=',.0f',) for c in columns]+[alt.Tooltip("date:T", title="Month",format = ("%b %Y"),)],
                             ).add_selection(hover)
 
     return (lines + points + tooltips).interactive()
@@ -39,9 +39,9 @@ def get_chart_cc(data):
     )
 
     lines = (
-        alt.Chart(data, title="Persentase Pertambahan Kasus Covid AS Dibandingkan Dengan Bulan Sebelumnya",height=400).mark_line().encode(
-            x=alt.X("date:T",title='Bulan'),
-            y=alt.Y('grwt_perc:Q',scale=alt.Scale(domain=[-100, 350]), title='Persentase (%)'),
+        alt.Chart(data, title="Month on Month Percentage Increase in US COVID-19 Cases",height=400).mark_line().encode(
+            x=alt.X("date:T",title='Month'),
+            y=alt.Y('grwt_perc:Q',scale=alt.Scale(domain=[-100, 350]), title='Percentage (%)'),
             color=alt.value("#FF5733"),
         )
     )
@@ -50,14 +50,14 @@ def get_chart_cc(data):
 
     tooltips = (
         alt.Chart(data).mark_rule().encode(
-            x=alt.X("date:T",title='Bulan'),
-            y=alt.Y('grwt_perc:Q',scale=alt.Scale(domain=[-100, 350]), title='Persentase (%)'),
+            x=alt.X("date:T",title='Month'),
+            y=alt.Y('grwt_perc:Q',scale=alt.Scale(domain=[-100, 350]), title='Percentage (%)'),
             opacity=alt.condition(hover, alt.value(0.3), alt.value(0)),
             tooltip=[
-                alt.Tooltip("date:T", title="Bulan",format = ("%b %Y")),
-                alt.Tooltip("grwt_perc:Q", title="Persentase",format=',.2f'),
-                alt.Tooltip("conf_cases:Q", title="Kasus Positif"),
-                alt.Tooltip("prev_ccs:Q", title="Kasus Bulan Sebelumnya"),
+                alt.Tooltip("date:T", title="Month",format = ("%b %Y")),
+                alt.Tooltip("grwt_perc:Q", title="Percentage",format=',.2f'),
+                alt.Tooltip("conf_cases:Q", title="Positive Cases Count"),
+                alt.Tooltip("prev_ccs:Q", title="Previous Cases Count"),
             ],
         )
         .add_selection(hover)
@@ -74,9 +74,9 @@ def get_chart_mob(data):
     )
 
     lines = (
-        alt.Chart(data, title="Perubahan Pengunjung Setiap Kategori Tempat Selama Pandemi COVID-19",height=400).mark_line().encode(
-            x=alt.X("date:T",title='Bulan'),
-            y=alt.Y('value:Q', title='Persentase % (rata-rata per bulan)',scale=alt.Scale(domain=[-55, 40])),
+        alt.Chart(data, title="Visitor Changes For Each Place Category During COVID-19 Pandemic",height=400).mark_line().encode(
+            x=alt.X("date:T",title='Month'),
+            y=alt.Y('value:Q', title='Percentage % (Avg per Month)',scale=alt.Scale(domain=[-55, 40])),
             color="variable:N",
             strokeDash="variable:N",
         )
@@ -84,11 +84,11 @@ def get_chart_mob(data):
 
     points = lines.transform_filter(hover).mark_circle(size=65)
 
-    base = alt.Chart(data).encode(x=alt.X("date:T",title='Bulan'))
+    base = alt.Chart(data).encode(x=alt.X("date:T",title='Month'))
     columns = sorted(data.variable.unique())
     tooltips = base.transform_pivot('variable', value='value', groupby=['date']).mark_rule().encode(
                     opacity=alt.condition(hover, alt.value(0.3), alt.value(0)),
-                    tooltip=[alt.Tooltip(c, type='quantitative',format=',.2f',) for c in columns]+[alt.Tooltip("date:T", title="Bulan",format = ("%b %Y"))],
+                    tooltip=[alt.Tooltip(c, type='quantitative',format=',.2f',) for c in columns]+[alt.Tooltip("date:T", title="Month",format = ("%b %Y"))],
                             ).add_selection(hover)
 
     return (lines + points + tooltips).interactive()
@@ -102,9 +102,9 @@ def get_chart_unp(data):
     )
 
     lines = (
-        alt.Chart(data, title="Persentase Pengangguran Per Bulan di AS",height=360).mark_line().encode(
+        alt.Chart(data, title="US Unemployment Rate",height=360).mark_line().encode(
             x=alt.X("date:T",title='Bulan'),
-            y=alt.Y('value:Q', title='Persentase % (Terhadap jumlah seluruh tenaga kerja)'),
+            y=alt.Y('value:Q', title='Percentage % (Of All Labor Force)'),
             color=alt.value("#2CA02C"),
         )
     )
@@ -113,12 +113,12 @@ def get_chart_unp(data):
 
     tooltips = (
         alt.Chart(data).mark_rule().encode(
-            x=alt.X("date:T",title='Bulan'),
-            y=alt.Y('value:Q', title='Persentase % (Terhadap jumlah seluruh tenaga kerja)'),
+            x=alt.X("date:T",title='Month'),
+            y=alt.Y('value:Q', title='Percentage % (Of All Labor Force)'),
             opacity=alt.condition(hover, alt.value(0.3), alt.value(0)),
             tooltip=[
-                alt.Tooltip("date:T", title="Bulan",format = ("%b %Y")),
-                alt.Tooltip("value:Q", title="Persentase",format=',.1f'),
+                alt.Tooltip("date:T", title="Month",format = ("%b %Y")),
+                alt.Tooltip("value:Q", title="Percentage",format=',.1f'),
             ],
         )
         .add_selection(hover)
@@ -135,9 +135,9 @@ def get_chart_day_mob(data):
     )
 
     lines = (
-        alt.Chart(data, title="Perubahan Pengunjung Setiap Kategori Tempat Selama Pandemi COVID-19 (Per Hari)",height=400).mark_line().encode(
-            x=alt.X("date:T",title='Tanggal'),
-            y=alt.Y('value:Q', title='Persentase (%)',scale=alt.Scale(domain=[-55, 40])),
+        alt.Chart(data, title="Visitor Changes For Each Place Category During COVID-19 Pandemic (Daily)",height=400).mark_line().encode(
+            x=alt.X("date:T",title='Date'),
+            y=alt.Y('value:Q', title='Percentage (%)',scale=alt.Scale(domain=[-55, 40])),
             color="variable:N",
             strokeDash="variable:N",
         )
@@ -145,10 +145,10 @@ def get_chart_day_mob(data):
 
     points = lines.transform_filter(hover).mark_circle(size=65)
 
-    base = alt.Chart(data).encode(x=alt.X("date:T",title='Tanggal'))
+    base = alt.Chart(data).encode(x=alt.X("date:T",title='Date'))
     columns = sorted(data.variable.unique())
     tooltips = base.transform_pivot('variable', value='value', groupby=['date']).mark_rule().encode(
                     opacity=alt.condition(hover, alt.value(0.3), alt.value(0)),
-                    tooltip=[alt.Tooltip(c, type='quantitative',format=',.2f') for c in columns]+[alt.Tooltip("date:T", title="Tanggal")],
+                    tooltip=[alt.Tooltip(c, type='quantitative',format=',.2f') for c in columns]+[alt.Tooltip("date:T", title="Date")],
                             ).add_selection(hover)
     return (lines + points + tooltips).interactive()
--- cp.py
+++ cp.py
@@ -17,48 +17,47 @@ def space(num_lines=1):
         st.write("")
 
 space(1)
-title = '<p style="text-align: center;font-size: 30px;"><strong>Perubahan <em>Work-life</em> di Amerika Serikat Pada Awal Pandemi COVID-19</strong></p>'
+title = '<p style="text-align: center;font-size: 30px;"><strong>Work-Life Changes in the United States During the Early COVID-19 Pandemic</strong></p>'
 st.markdown(title, unsafe_allow_html=True)
 
-"""Tahun 2020 jelas akan menjadi tahun yang tidak akan pernah terlupakan. Tahun tersebut masih meninggalkan kenangan, peristiwa dan rasa haru yang mungkin masih membekas dalam pikiran
-karena COVID-19. Pandemi tersebut telah banyak mempengaruhi aspek kehidupan semua manusia di dunia. Bahkan Amerika Serikat yang telah lama disebut sebagai Negara Adikuasa pun 
-tidak dapat menghindar dari efek pandemi satu ini. Negara Adikuasa tersebut malah terpukul cukup telak saat kasus terkonfimasi yang terjadi disana mulai meledak.
-Salah satu indikator yang menunjukkan hal tersebut adalah PCE(*Personal Consumption Expenditures*)  Amerikat Serikat selama masa pandemi COVID-19."""
+"""The year 2020 will undoubtedly be a year that will never be forgotten. That year still leaves behind memories, events, and emotions 
+that may remain etched in our minds due to COVID-19. The pandemic has significantly impacted various aspects of life for people worldwide. Even the United States, 
+long regarded as a superpower, could not escape the effects of this pandemic. One indicator that reflects this is the U.S. Personal Consumption Expenditures (PCE) during the COVID-19 pandemic."""
 
 space(1)
 
 col1, col2=st.columns([1,2])
 with col1 :
     space(11)
-    """PCE(Personal Consumption Expenditures) menunjukkan  total pengeluaran yang dikeluarkan oleh seluruh rumah tangga di Amerika Serikat (AS) untuk 
-    kebutuhan sehari-hari yang dapat berupa makanan, produk berupa layanan, biaya transportasi dan barang untuk jangka waktu pendek maupun panjang. 
-    Tren PCE AS selalu meningkat dari tahun ke tahun namun saat pandemi mulai meledak, PCE menurun secara tajam yang mencapai titik terendah pada bulan April 2020.
-    Kita juga dapat membandingkan PCE yang terjadi selama masa pandemi dengan perkiraan PCE jika tidak terjadi pandemi COVID-19."""
+    """PCE (Personal Consumption Expenditures) represents the total expenditures made 
+    by all households in the United States for daily needs, which can include food, service-based products, transportation costs, and goods. 
+    The US PCE trend had always increased year on year, but when the pandemic began to surge, the PCE dropped sharply, 
+    reaching it's lowest point in April 2020. We can also compare the PCE during the pandemic with the estimated PCE if the COVID-19 pandemic had not occurred."""
 with col2 : 
-    st.info("""#### PCE Amerika Serikat Terjun Bebas Saat Awal Pandemi COVID-19""")
-    tab1, tab2 = st.tabs(["2018-Sekarang", "Tabel"])
+    st.info("""#### The US PCE Dropped Sharply During Early COVID-19 Pandemic""")
+    tab1, tab2 = st.tabs(["2018-Present", ""])
     with tab1:
-        text = '<p style="font-size: 10px;"><strong>*Prediksi PCE dibuat berdasar data PCE Amerika Serikat Selama 2010-2019</strong></p>'
+        text = '<p style="font-size: 10px;"><strong>*PCE Prediction based on United States PCE Data during 2010-2019</strong></p>'
         st.markdown(text, unsafe_allow_html=True)
         source=df_pce[(df_pce['date']>'2018')&(df_pce['date']<'2022-05')]
         chart = get_chart_pce(source)
         st.altair_chart(chart, use_container_width=True)
         
-    with tab2:
-       "Berikut ini tabel yang menunjukkan nilai PCE AS sejak tahun 1959 (dalam miliar USD)"
-       df_pce_us_ft=df_pce[df_pce['variable']=='PCE'].set_index('date')
-       df_pce_us_ft=df_pce_us_ft['value']
-       df_pce_us_ft.columns=['PCE']
-       st.dataframe(df_pce_us_ft.astype(int))
-    st.write("&emsp;&emsp;Sumber: [*https://fred.stlouisfed.org/*](https://fred.stlouisfed.org/series/PCE)")
+    # with tab2:
+    #    "Table below shows US PCE from 1959 (Billion USD)"
+    #    df_pce_us_ft=df_pce[df_pce['variable']=='PCE'].set_index('date')
+    #    df_pce_us_ft=df_pce_us_ft['value']
+    #    df_pce_us_ft.columns=['PCE']
+    #    st.dataframe(df_pce_us_ft.astype(int))
+    st.write("&emsp;&emsp;Source: [*https://fred.stlouisfed.org/*](https://fred.stlouisfed.org/series/PCE)")
 
-"""Mungkin kita akan mengira bahwa PCE AS menurun dikarenakan adanya lockdown yang dimulai pada bulan Maret 2020 sehingga masyarakat di AS mengurangi mobilitas yang kemudian mengurangi biaya transportasi serta biaya
-    di luar rumah lainnya. Namun apakah hanya itu saja yang mempengaruhi menurunnya PCE di AS?"""
+"""We might assume that the US PCE dropped due to the lockdown in March 2020, 
+which led people in the US to reduce their mobility, thereby lowering transportation costs and other out-of-home expenses. But is this true?"""
 space(1)
 col1, col2=st.columns([2,1])
 with col1 :
-    st.info("""#### Pengangguran di AS Meningkat Tajam Saat Awal Pandemi COVID-19""")
-    tab1, tab2 = st.tabs(["2018-Sekarang", "1960-Sekarang"])
+    st.info("""#### US Unemployment rate Increased Significantly on The Early COVID-19 Pandemic""")
+    tab1, tab2 = st.tabs(["2018-Present", "1960-Present"])
     with tab1:
         source4=df_unp_us[(df_unp_us['date']>'2018')&(df_unp_us['date']<'2022-05')]
         chart=get_chart_unp(source4)
@@ -68,20 +67,17 @@ with col1 :
         source4=df_unp_us[(df_unp_us['date']>'1960')&(df_unp_us['date']<'2022-05')]
         chart=get_chart_unp(source4)
         st.altair_chart(chart, use_container_width=True)
-    st.write("&emsp;&emsp;Sumber: [*https://data.oecd.org/*](https://data.oecd.org/unemp/unemployment-rate.htm#indicator-chart)")
+    st.write("&emsp;&emsp;Source: [*https://data.oecd.org/*](https://data.oecd.org/unemp/unemployment-rate.htm#indicator-chart)")
 
 with col2 :
     space(15)
-    """Tingkat pengangguran di AS juga meningkat tajam saat awal pandemi COVID-19 yang juga mencapai titik tertinggi di bulan april 2020.
-    Pengeluaran yang berkurang kemungkinan besar juga terjadi akibat banyak dari mereka yang kehilangan pekerjaannya. 
-    Bahkan tingkat pengangguran ini menjadi yang tertinggi dalam sejarah 
-    Amerika serikat selama lebih dari 60 tahun terakhir!"""
+    """The US Unemployment rate increased significantly during the early COVID-19 Pandemic and reaching it's highest point in April 2020. This point also marked an all-time high for over last 60 years.
+    Expenditures likely decreased due to many US citizens losing their jobs."""
       
-st.warning("""Persamaan antara grafik PCE and tingkat pengangguran di AS adalah tren melandai yang terjadi setelah mengalami perubahan 
-                yang cukup tajam di bulan April 2020 dan semakin mendekati normal menjelang tahun 2022.""")
+st.warning("""The similarity between US PCE Trends and the unemployment rate is the gradual decrease after the significant changes in April 2020 and approaching normal levels by the year 2022.""")
         
 
-with st.expander("Klik untuk melihat kembali grafik PCE dan Tingkat pengangguran di AS"):
+with st.expander("Click to show PCE Trends and the unemployment rate again"):
     col1, col2=st.columns([1,1])
     with col1 :
         source=df_pce[(df_pce['date']>'2018')&(df_pce['date']<'2022-05')]
@@ -92,24 +88,24 @@ with st.expander("Klik untuk melihat kembali grafik PCE dan Tingkat pengangguran
         source4=df_unp_us[(df_unp_us['date']>'2018')&(df_unp_us['date']<'2022-05')]
         chart=get_chart_unp(source4)
         st.altair_chart(chart, use_container_width=True)
-"""Apakah hal ini berarti masyarakat AS mulai menjalani kehidupan (terutama dalam hal pekerjaan) yang semakin mendekati normal seperti dahulu?""" 
-"""Untuk mengetahui hal tersebut kita akan melihat grafik pola mobilitas masyarakat AS yang diukur berdasar perubahan pengunjung pada setiap kategori tempat."""
+"""Does this mean that US citizens are returning to their normal lives (especially their work lives) as before?""" 
+"""We will analyze US Citizens mobility trends on various locations to validate our assumptions."""
 
 space(1)
 
 col1, col2=st.columns([1,2])
 with col2:
-    st.info("""#### Perubahan Mobilitas Masyarakat AS Selama Pandemi""")
+    st.info("""#### US Citizens Mobility Trends During Pandemic""")
     source3=df_mon_mob[(df_mon_mob['date']<'2022-07')&(df_mon_mob['code']=='USA')]
     source3=source3[['date','variable','value']]
     all_symbols = source3.variable.unique()
-    symbols = st.multiselect("Pilih Kategori Untuk Divisualisasikan", all_symbols, all_symbols)
+    symbols = st.multiselect("Choose Category to Visualize", all_symbols, all_symbols)
 
-    chc = st.checkbox('Tampilkan juga perubahan pengunjung per hari')
-    text = '<p style="font-size: 11px;"><strong>*NRM ditentukan berdasarkan median pengunjung pada 03 Januari 2020 sampai 06 Februari 2020</strong></p>'
+    chc = st.checkbox('Show Trends Per Day')
+    text = '<p style="font-size: 11px;"><strong>*NRM determined based on median visitors on 03 January 2020 Until 06 February 2020</strong></p>'
     st.markdown(text, unsafe_allow_html=True)
     if chc :
-        tab1, tab2=st.tabs(["Rataan Per Bulan", "Per Hari"])
+        tab1, tab2=st.tabs(["Average Per Month", "Per Day"])
         with tab1 :
             source = source3[source3.variable.isin(symbols)]
             for i in source.date.unique():
@@ -133,39 +129,38 @@ with col2:
             source=pd.concat([source,add])
         chart = get_chart_mob(source)
         st.altair_chart(chart, use_container_width=True)
-    st.write("&emsp;&emsp;Sumber: [*https://ourworldindata.org/*](https://ourworldindata.org/covid-google-mobility-trends)")
+    st.write("&emsp;&emsp;Source: [*https://ourworldindata.org/*](https://ourworldindata.org/covid-google-mobility-trends)")
 with col1:
     space(5)
-    text = '<p style="font-size: 12px;"><strong>KTR : Kantor, tempat kerja dan sejenisnya</strong></p>'
+    text = '<p style="font-size: 12px;"><strong>KTR : Workplaces</strong></p>'
     st.markdown(text, unsafe_allow_html=True)
-    text = '<p style="font-size: 12px;"><strong>TP&ensp;&nbsp;: Stasiun, bandara dan tempat transportasi publik lainnya</strong></p>'
+    text = '<p style="font-size: 12px;"><strong>TP&ensp;&nbsp;: Public transport stations</strong></p>'
     st.markdown(text, unsafe_allow_html=True)
-    text = '<p style="font-size: 12px;"><strong>RCF : Restaurant, cafe, bioskop dan sejenisnya</strong></p>'
+    text = '<p style="font-size: 12px;"><strong>RCF : Restaurants, cafes, movie theaters, etc.</strong></p>'
     st.markdown(text, unsafe_allow_html=True)
-    text = '<p style="font-size: 12px;"><strong>SAP : Supermarket, pasar, toko dan sejenisnya</strong></p>'
+    text = '<p style="font-size: 12px;"><strong>SAP : Grocery and Pharmacy Stores</strong></p>'
     st.markdown(text, unsafe_allow_html=True)
-    text = '<p style="font-size: 12px;"><strong>NRM : Nilai awal sebelum pandemi</strong></p>'
+    text = '<p style="font-size: 12px;"><strong>NRM : Normal baseline before pandemic emerged</strong></p>'
     st.markdown(text, unsafe_allow_html=True)
 
     space(3)
-    """Semua kategori mempunyai pola fluktuatif yang hampir sama namun memiliki rentang nilai yang berbeda. 
-    Fluktuatif yang terjadi pada awal dan akhir tahun 2020-2021 serta 2021-2022 terjadi akibat musim dingin di AS
-    sehingga masyrakat AS cenderung untuk tinggal di dalam rumah dibandingkan hari lainnya. 
-    Kita pun dapat melihat kalau kategori KTR dan TP tidak pernah dalam sehari pun menyentuh titik normal seperti sebelum pandemi."""
+    """All category show the same trend patterns, with only a difference in the range of value.
+    Fluctuating events that occurred between late 2020 to early 2021 and late 2021 to early 2022 were caused by the winter season in the US, 
+    which led to citizens tending to stay at home. Additionally, we can observe that the KTR and TP categories did not reach their normal values, not even for a single day."""
 
 
-st.warning("Sambil mengingat kembali grafik tingkat pengangguran yang terus berkurang menandakan semakin banyaknya *WFH* (*Work From Home*)")
-st.write("Lalu mengapa PCE AS juga terus meningkat saat masyarakat AS banyak yang *WFH* (berkurangnya biaya transportasi)? Hal tersebut terjadi karena masyarakat di AS telah mempunyai *spending habit* yang berbeda selama masa pandemi[[1]](https://www.boj.or.jp/en/research/wps_rev/rev_2020/rev20e07.htm/)[[2]](https://www2.deloitte.com/us/en/insights/economy/us-consumer-spending-after-covid.html).")
-"""Masyarakat AS cenderung mengurangi layanan produk berupa servis dan kebutuhan tersier namun menambah pengeluaran untuk kebutuhan pokok.
-        Hal tersebut cukup terlihat pada kategori SAP yang beberapa kali mengalami peningkatan dibandingkan masa sebelum pandemi."""
+st.warning("As we already know that unemployment rate decreased along the time means that US citizens change over to *WFH* (*Work From Home*)")
+st.write("Then why did the PCE trends still increase while US citizens shifted to *WFH* (transportations cost reduced)? This was caused by a change in spending habit among US citizens after the pandemic starts[[1]](https://www.boj.or.jp/en/research/wps_rev/rev_2020/rev20e07.htm/)[[2]](https://www2.deloitte.com/us/en/insights/economy/us-consumer-spending-after-covid.html).")
+"""US citizens tends to reduce their cost on service-based products and increased their spend on basic needs.
+        That's why we can see that SAP category increases some times compared to normal value before pandemic."""
 
-with st.expander("Bagaimana dengan negara lain? Klik disini!"):
-    symbols2 = st.multiselect("Pilih Kategori Untuk Divisualisasikan", all_symbols, ['TP','KTR'],key=1)
+with st.expander("Other countries? Click Here!"):
+    symbols2 = st.multiselect("Choose Category to Visualizen", all_symbols, ['TP','KTR'],key=1)
     col1, col2=st.columns([1,1])
     with col1 :
-        option1 = st.selectbox('Pilih Negara (1):',
+        option1 = st.selectbox('Choose Country (1):',
         ('United States','Canada','United Kingdom','France', 'Singapore','Japan','Australia'))
-        tab1, tab2=st.tabs(["Rataan Per Bulan", "Per Hari"])
+        tab1, tab2=st.tabs(["Average Per Month", "Per Day"])
         with tab1 :
             source3=df_mon_mob[(df_mon_mob['date']<'2022-07')&(df_mon_mob['country']==option1)]
             source3=source3[['date','variable','value']]
@@ -186,9 +181,9 @@ with st.expander("Bagaimana dengan negara lain? Klik disini!"):
             st.altair_chart(chart, use_container_width=True)
 
     with col2 :
-        option2 = st.selectbox('Pilih Negara (2):',
+        option2 = st.selectbox('Choose Country (2):',
         ('Indonesia','Brazil','Mexico','Poland','Hungary','India','South Africa'))
-        tab1, tab2=st.tabs(["Rataan Per Bulan", "Per Hari"])
+        tab1, tab2=st.tabs(["Average Per Month", "Per Day"])
         with tab1 :
             source3=df_mon_mob[(df_mon_mob['date']<'2022-07')&(df_mon_mob['country']==option2)]
             source3=source3[['date','variable','value']]
@@ -208,15 +203,14 @@ with st.expander("Bagaimana dengan negara lain? Klik disini!"):
             chart = get_chart_day_mob(source)
             st.altair_chart(chart, use_container_width=True)
 
-    """Masyarakat di negara-negara berkembang cenderung lebih cepat kembali pada keadaan semula atau bahkan meningkat dalam hal *WFO (Work From Office)*.
-       Sedangkan masyarakat di negara-negara maju cenderung mulai nyaman dengan *WFH (Work From Home)* meskipun ada kemungkinan akan
-       mendekati normal seiring berjalan waktu namun lebih lambat dibandingkan dengan negara-negara berkembang. Semua itu dapat
-       kita lihat pada kategori TP dan KTR."""
-    """Pada kategori RCF dan SAP pun juga memiliki pola yang sama namun lebih cepat terjadi dibandingkan dengan kategori TP dan KTR. Semua itu
-    kemungkinan disebabkan karena adanya perbedaan kemudahan ataupun kemurahan dalam melakukan segala sesuatu secara online pada negara maju dan negara berkembang."""
+    """Citizens in developing countries tends to return to their previous state in terms of *WFO (Work From Office)*.
+       Meanwhile citizens in developed countries tend to become more comfortable with WFH (Work From Home), although there is a possibility that it will approach normalcy over time, 
+       but at a slower pace compared to developing countries. All of this can be observed in the TP and KTR categories."""
+    """On RCF and SAP categories also have a same patterns but with a faster pace than TP and KTR. 
+       All of this is likely caused by the differences in the ease and affordability of doing things online among those countries."""
 
 space(3)
-end = '<p style="text-align: center;font-size: 14px;"><strong>Pragasto Aji Hendro Puadi - Tetris Capstone Project</strong></p>'
+end = '<p style="text-align: center;font-size: 14px;"><strong>Pragasto Aji Hendro Puadi</strong></p>'
 st.markdown(end, unsafe_allow_html=True)
 
 hide_streamlit_style = """
